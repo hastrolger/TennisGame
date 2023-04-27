6d8e29fd446c2067d2954e6eb3f93930a82ae0d6
hastrolger <parralesjael@gmail.com>
2023-04-27 11:26:40 -0700

    Development finished

--- Program.cs
+++ Program.cs
@@ -2,13 +2,49 @@
 int menuOptionSelected = 0;
 bool menuOptionIsOk = false;
 
+// generate static score data
+List<String> listOfScores = new List<String>(){"0", "15", "30", "40"};
+Dictionary<Players, List<String>> playerScores = new Dictionary<Players, List<String>>();
+
+playerScores.Add(Players.P1, new List<string>(){"0","30"});
+playerScores.Add(Players.P2, new List<string>(){"40","30"});
+
 while(menuOptionIsOk != true){
     ShowMainMenu();
     menuOptionSelected = GetMenuOption();
     menuOptionIsOk = ValidateMenuOptionSelected(menuOptionSelected);
-
+    
     Console.ReadLine();
     Console.Clear();
+}
+
+GetGameResults();
+
+// get results of the game 
+void GetGameResults() {
+    int pointsForP1 = 0;
+    int pointsForP2 = 0;
+    
+    var listOfScoresP1 = playerScores[Players.P1];
+    var listOfScoresP2 = playerScores[Players.P2];
+
+    for (int i = 0; i <= listOfScoresP1.Count - 1; i++){
+        if (Int32.Parse(listOfScoresP1[i]) >  Int32.Parse(listOfScoresP2[i]))
+        {
+            pointsForP1 += 1;
+        }
+        if(Int32.Parse(listOfScoresP1[i]) < Int32.Parse(listOfScoresP2[i])) {
+            pointsForP2 += 1;
+        }
+    }
+
+    if (pointsForP1 > pointsForP2){
+        Console.WriteLine($"Winner: {Players.P1}, points: {pointsForP2}");
+    } else if (pointsForP2 > pointsForP1) {
+        Console.WriteLine($"Winner: {Players.P2}, points: {pointsForP2}");
+    } else {
+        Console.WriteLine("Game matched");
+    }
 }
 
 // show main menu
@@ -23,7 +59,7 @@ void ShowMainMenu() {
 // get menu option and return it as an integer
 int GetMenuOption() {
     Console.WriteLine("Select menu option: ");
-    int input = Convert.ToInt16(Console.ReadLine());
+    int input = Convert.ToInt16(Console.ReadLine());    
 
     return input;
 }
@@ -31,9 +67,9 @@ int GetMenuOption() {
 // validate menu option and return true if it's in range 
 bool ValidateMenuOptionSelected(int menuOption) {
     bool isOk = false;
-    int MenuOptionLength = Enum.GetValues(typeof(MainMenu)).Length;
+    int MenuLength = Enum.GetValues(typeof(MainMenu)).Length;
 
-    if(menuOption < 1 || menuOption > MenuOptionLength) {
+    if(menuOption < 1 || menuOption > MenuLength) {
         Console.WriteLine("Option is out of range.");
         isOk = false;
     }
@@ -49,3 +85,7 @@ enum MainMenu {
     Exit = 2
 }
 
+enum Players {
+    P1 = 1,
+    P2 = 2
+}
